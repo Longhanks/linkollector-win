72c7b18487c6e5272233df3e0676050801c3e04f
Andreas Schulz <andi.schulz@me.com>
2020-04-28 19:13:50 +0200

    Formatting

--- src/constants.h
+++ src/constants.h
@@ -2,14 +2,13 @@
 
 #include <cstddef>
 
- constexpr const int WINDOW_CLASS_NAME_MAX_SIZE = 256;
+constexpr const int WINDOW_CLASS_NAME_MAX_SIZE = 256;
 
- constexpr const wchar_t *MAIN_WINDOW_CLASS_NAME =
-    L"Linkollector Main Window";
+constexpr const wchar_t *MAIN_WINDOW_CLASS_NAME = L"Linkollector Main Window";
 
- constexpr const std::size_t LINE_PADDING = 14;
+constexpr const std::size_t LINE_PADDING = 14;
 
- constexpr const int LINE_HEIGHT = 1;
+constexpr const int LINE_HEIGHT = 1;
 
 constexpr const int WINDOW_WIDTH_96 = 640;
 constexpr const int WINDOW_HEIGHT_96 = 480;
--- src/line.h
+++ src/line.h
@@ -3,8 +3,8 @@
 #include <Windows.h>
 
 LRESULT CALLBACK line_window_f(HWND hwnd,
-                              UINT message_code,
-                              WPARAM w_param,
-                              LPARAM l_param,
-                              UINT_PTR subclass_id,
-                              DWORD_PTR ref_data) noexcept;
+                               UINT message_code,
+                               WPARAM w_param,
+                               LPARAM l_param,
+                               UINT_PTR subclass_id,
+                               DWORD_PTR ref_data) noexcept;
